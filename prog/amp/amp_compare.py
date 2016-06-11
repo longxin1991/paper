3b8aadc6b7ca418117444cab4a5135f0924b1101
longxin1991 <longxin13@mails.ucas.ac.cn>
2016-06-11 18:15:48 +0800

    update file, align wf by max amplitude.

--- prog/amp/amp_compare.py
+++ prog/amp/amp_compare.py
@@ -5,7 +5,23 @@ from plotsection import GetStream
 from wfutils import sum_traces
 import pylab as pl
 from sys import argv
+import numpy as np
 
+def align_acd_maxamp(data,delta,pre=10,post=10):
+
+    tmp = np.abs(data)
+    ind = tmp.argmax()
+    print("avg amp:{0:f}".format(data[ind]))
+    b = int(ind - round(pre/delta))
+    e = int(ind + round(post/delta))
+    seg = data[b:e]
+    return seg
+
+def set_ticklabel_size(axes,size):
+    for tick in axes.xaxis.get_major_ticks():
+        tick.label.set_fontsize(size)
+    for tick in axes.yaxis.get_major_ticks():
+        tick.label.set_fontsize(size)
 
 if __name__ == '__main__':
     if len(argv) < 3:
@@ -21,22 +37,29 @@ if __name__ == '__main__':
     s1,t1 = sum_traces(st1)
     s2,t2 = sum_traces(st2)
 
-
+    d1 = align_acd_maxamp(s1,t1[1]-t1[0])
+    d2 = align_acd_maxamp(s2,t2[1]-t2[0])
+    npts = len(d1)
+    t = np.arange(npts)*(t1[1]-t1[0]) - 10.0
 
     ax1 = pl.subplot(211)
-    ax1.plot(t1,s1,'k')
-    ax1.set_xlim(xmin=t1[0])
+    ax1.plot(t,d1,'k')
+    ax1.set_xlim(xmin=t[0]-0.1,xmax=t[-1]+0.1)
 
     ax1.locator_params(axis='y',prune='both',tight=True)
     ax1.minorticks_on()
 
+
     ax2 = pl.subplot(212)
-    ax2.plot(t2,s2,'k')
-    ax2.set_xlim(xmin=t2[0])
+    ax2.plot(t,d2,'k')
+    ax2.set_xlim(xmin=t[0]-0.1,xmax=t[-1]+0.1)
     ax2.locator_params(axis='y',prune='both',tight=True)
     ax2.minorticks_on()
-    ax2.set_xlabel('Time[s]')
-    
+    ax2.set_xlabel('Time[s]',fontsize=20)
+   
+    set_ticklabel_size(ax1,16)
+    set_ticklabel_size(ax2,16)
+
     pl.tight_layout()
     #pl.show()
     pl.savefig('amp_comp.eps',bbox_inches='tight')
